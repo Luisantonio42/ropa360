411bbbca4c54d01f8c6a910a55750a0ff3094c91
Luis Garcia <luisgarmir@hotmail.com>
2021-12-21 17:42:39 -0600

    broken homepage into components and finished styling menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-import HomePage from './HomePage';
+import HomePage from './pages/homepage/HomePage';
 
 function App() {
   return (
-    <div >
+    <div className='body'>
       <HomePage/>
     </div>
   );
