2efbc8a06419b1e86fb0b46e4df0626cd13c3368
Luis <luisgarmir@hotmail.com>
2022-01-17 12:15:28 -0600

    Implemented firebase utils , included hability to add users by email and password

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch , Route } from 'react-router';
 import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/Header';
 import SignInSignUpPage from './pages/sign-in-sign-up/SignInSignUpPage';
-import {auth} from './firebase/FireBaseUtils';
+import {auth, createUserProfileDocument} from './firebase/FireBaseUtils';
 
 class App extends React.Component {
   
@@ -20,8 +20,21 @@ class App extends React.Component {
   unsuscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsuscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        console.log(this.state);
+        });
+      }
+      this.setState({currentUser: userAuth});
+
     });
   }
 
--- src/pages/sign-in-sign-up/SignInSignUpPage.jsx
+++ src/pages/sign-in-sign-up/SignInSignUpPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import './SignInSignUpPage.scss';
 import SignIn from "../../components/sign-in/SignIn";
+import SignUp from "../../components/sign-up/SignUp";
 
 const SignInSignUpPage = () => {
   return (
     <div className="sign-in-sign-up">
       <SignIn/>
+      <SignUp/>
     </div>
 
   );
